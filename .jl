d471863fdbe746c19be6dc8cca7cc59510cde136
exxjob <92322095+exxjob@users.noreply.github.com>
2023-05-02 02:57:58 +0300

    updated faulty syntax, debugging needed

--- .jl
+++ .jl
@@ -1,7 +1,6 @@
-const duplicateCharactersAllowed = false;
-# const illegalCharacters;
-function radicesConvert(input::Array{T,1}, inRadix::Array{T,1}, outRadix::Array{T,1})
-    function add (x::Array{Int,1}, y::Array{Int,1}, base::Int)
+
+function radicesConvert(input::Array{Char,1}, inRadix::Array{Char,1}, outRadix::Array{Char,1})
+    function add(x::Array{Int,1}, y::Array{Int,1}, base::Int)
         z::Array{Int,1}
         n::Int = max(x.length,length(y))
         carry::Int = 0
@@ -14,10 +13,9 @@ function radicesConvert(input::Array{T,1}, inRadix::Array{T,1}, outRadix::Array{
             carry = (zi / base).floor()
             i+=1
         end
+        return z
     end
-
-
-    function multiplyByNumber (num::Int, power::Array{Int,1}, base::Int)
+    function multiplyByNumber(num::Int, power::Array{Int,1}, base::Int)
         if num==0
             return [0]
         end
@@ -34,29 +32,30 @@ function radicesConvert(input::Array{T,1}, inRadix::Array{T,1}, outRadix::Array{
         end
         return result
     end
-  
+
     # creates an array based on the input with the indexes of the corresponding values on inRadix
-    function decodeInput ()
-        result::Array{Int,1}
-        for i in input.length:1
+    function decodeInput()
+        result::Array{Int,1}=[]
+        for i in length(input):1
             result.append!(inRadix.find(input[i]))
         end
         return result
     end
 
+    fromBase = length(inRadix)
+    toBase = length(outRadix)
     digits = decodeInput()
-    outArray::Array{Int,1}
-    power::Array{Int,1}[1]
+    outArray::Array{Int,1}=[]
+    power::Array{Int,1}=[1]
 
-    for i in 1:digits.length
-        outArray = add()
-        power = multiplyByNumber()
+    for i in 1:length(digits)
+        outArray = add(outArray,multiplyByNumber(digits[i],power,toBase),toBase)
+        power = multiplyByNumber(fromBase,power,toBase)
     end
 
-    out::Array{T,1}
-    for i in outArray:1
+    out::Array{Char,1}=[]
+    for i in length(outArray):-1:1
         out.append!(outRadix[outArray[i]])
     end
     return out
-
 end
